8ee7cc8f365b6994ccd3dbb7af758a697c9b6ee1
ssaiganesh <shankarsaiganesh@gmail.com>
2021-08-15 21:16:21 +0800

    Uploaded to pythonanywhere

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 
 from decouple import config
 import telebot
+import yfinance as yf
 
 API_KEY = config('API_KEY')
 bot = telebot.TeleBot(API_KEY)
@@ -8,17 +9,37 @@ bot = telebot.TeleBot(API_KEY)
 @bot.message_handler(commands=['greet'])
 def greet(message):
     bot.reply_to(message, "Hey!How's it going?")
-
+"""
 #Reply with the same message sent to the person
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
 	bot.reply_to(message, message.text)
-
+"""
 
 # This won't be sent as a reply to the command
 @bot.message_handler(commands=['hello'])
 def hello(message):
     bot.send_message(message.chat.id, "Hello!")
+
+def stock_request(message):
+  request = message.text.split()
+  if len(request) < 2 or request[0].lower() not in "price":
+    return False
+  else:
+    return True
+
+@bot.message_handler(func=stock_request)
+def send_price(message):
+  request = message.text.split()[1]
+  data = yf.download(tickers=request, period='5m', interval='1m')
+  if data.size > 0:
+    data = data.reset_index()
+    data["format_date"] = data['Datetime'].dt.strftime('%m/%d %I:%M %p')
+    data.set_index('format_date', inplace=True)
+    print(data.to_string())
+    bot.send_message(message.chat.id, data['Close'].to_string(header=False))
+  else:
+    bot.send_message(message.chat.id, "No data!")
 
 bot.polling()
 
